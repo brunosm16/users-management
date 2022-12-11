a1521ad9578cb13f68ab0c794ba61c45cefee685
brunosm16 <brunoskr23@gmail.com>
2022-12-11 11:19:31 -0300

    feat: add jwt-utils

---
+++ src/utils/jwt-utils.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import environment from '../config/environment.js';
+
+const { jwtAccessToken, jwtRefreshToken } = environment;
+
+const DEFAULT_EXPIRATION = '1d';
+
+const generateAccessToken = (payload, options = {}) => {
+  const { expiresIn = DEFAULT_EXPIRATION } = options;
+
+  return jwt.sign(payload, jwtAccessToken, { expiresIn });
+};
+
+const generateRefreshToken = (payload) => {
+  return jwt.sign(payload, jwtRefreshToken);
+};
+
+const verifyAccessToken = (accessToken) => {
+  return jwt.verify(accessToken, jwtAccessToken);
+};
+
+const verifyRefreshToken = (refreshToken) => {
+  return jwt.verify(refreshToken, jwtRefreshToken);
+};
+
+module.exports = { generateAccessToken, generateRefreshToken, verifyAccessToken, verifyRefreshToken };
